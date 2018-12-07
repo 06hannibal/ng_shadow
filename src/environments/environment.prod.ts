63e89ffb8b4ce317a3823503b93294362aff4675
06hannibal <06hannibal@gmail.com>
2018-12-07 12:16:29 +0200

    edit .gitignore

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
-import {HttpHeaders} from '@angular/common/http';
+// import {HttpHeaders} from '@angular/common/http';
 
 export const environment = {
   production: true,
   mainUrl : 'http://localhost:4200/',
-  httpjson : {headers: new HttpHeaders({
-            'Content-Type': 'application/json'
-        })}
+  // httpjson : {headers: new HttpHeaders({
+  //           'Content-Type': 'application/json'
+  //       })}
 };
