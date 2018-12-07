28381ff155fb20b07c59b859e2dd32523a986dd1
06hannibal <06hannibal@gmail.com>
2018-12-07 12:19:16 +0200

    edit environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,14 +1,14 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-// import { HttpHeaders } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
 
 export const environment = {
   production: false,
   mainUrl : 'http://shadow.com/',
-  // httpjson : {headers: new HttpHeaders({
-  //         'Content-Type': 'application/json'
-  //     })}
+  httpjson : {headers: new HttpHeaders({
+          'Content-Type': 'application/json'
+      })}
 };
 
 /*
